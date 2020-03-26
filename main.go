a5fc6c001f1ebe2f89ddecaf9affe942eb830963
MirambekNagashbekov <n.mirambek@gmail.com>
2020-03-26 10:41:02 +0600

    initial upload

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func main() {
+	numberCores := runtime.NumCPU()
+	runtime.GOMAXPROCS(numberCores)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	scanner := bufio.NewScanner(os.Stdin)
+	var processedList = make(map[int]int)
+	var result = make([]string, 0)
+	var mutex = &sync.Mutex{}
+	url := make(chan string, numberCores)
+	counter := 0
+	for ii := 0; ii < numberCores; ii++ {
+		go func(urlCh <-chan string, id int) {
+			for {
+				url, more := <-urlCh
+				if more == false {
+					return
+				}
+				start := time.Now()
+				resp, err := http.Get(url)
+				mutex.Lock()
+				processedList[id]++
+				mutex.Unlock()
+				if err != nil {
+					mutex.Lock()
+					result = append(result, err.Error())
+					mutex.Unlock()
+					return
+				}
+				defer resp.Body.Close()
+				body, _ := ioutil.ReadAll(resp.Body)
+				secs := time.Since(start).Seconds()
+				mutex.Lock()
+				result = append(result, fmt.Sprintf("%s;%d;%d;%.2fms", url, resp.StatusCode, len(body), secs*1000))
+				mutex.Unlock()
+			}
+		}(url, ii)
+	}
+S:
+	for scanner.Scan() {
+		select {
+		case <-interrupt:
+			break S
+		default:
+			urlText := scanner.Text()
+			if len(urlText) == 0 {
+				break S
+			}
+			url <- urlText
+			counter++
+		}
+	}
+	if scanner.Err() != nil {
+		fmt.Println(scanner.Err())
+	}
+	mutex.Lock()
+	for _, v := range result {
+		fmt.Printf("%s\n", v)
+	}
+	mutex.Unlock()
+	close(url)
+	mutex.Lock()
+	fmt.Println("<порядковый номер паралельного потока запросов>:<число запросов>")
+	for k, v := range processedList {
+		fmt.Printf("%d:%d\n", k, v)
+	}
+	mutex.Unlock()
+}
